9507dda411ffd00a10175a1452b5ab296807aa74
David Henderson <dahenderson@csumb.edu>
2018-09-01 22:56:25 +0000

    adds a helper for making responsive links on the home page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def nav_link(link_text, link_path)
+    cur = current_page?(link_path)
+    li_class_name = cur ? 'nav-item active px-lg-4' : 'nav-item px-lg-4'
+    a_class_name = 'nav-link text-uppercase text-expanded'
+    txt = cur ? content_tag(:span, "(current)", :class=>'sr-only') : nil
+    content_tag(:li, :class => li_class_name) do
+      link_to link_path, :class => a_class_name do
+        raw(link_text + txt.to_s)
+      end
+    end
+  end
 end
+
