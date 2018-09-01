51d5cb0f855d81ce7911ac3a2c8f26d8a57ea3b4
cayuse <cayuse@deepbondi.net>
2018-09-01 12:31:33 -0700

    testing default route

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -1,3 +1,5 @@
 class Competition < ApplicationRecord
-  belongs_to :owner
+  belongs_to :owner, :class_name => 'User', :foreign_key => 'user_id'
+    
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   resources :events
   resources :challenges
   resources :competitions
-  devise_for :users
+  # devise_for :users
+  devise_scope :user do
+      root to: "devise/sessions#new"
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
