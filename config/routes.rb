43adeab9a4906458222ac6d3fd6965ba7b717b7a
David Henderson <dahenderson@csumb.edu>
2018-09-11 01:17:49 +0000

    fixes the login issue

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,19 @@
 Rails.application.routes.draw do
   get "/pages/:page" => "pages#show"
-  root to: redirect('/users/sign_in')
+  #root to: redirect('/users/sign_in')
   devise_for :users, :controllers => { registrations: 'registrations' }
   devise_scope :user do
     authenticate :user do
       resources :events
       resources :challenges
       resources :competitions
-      root 'competitions#index'
       resources :users
+    end
+    authenticated :user do
+      root 'competitions#index'
+    end
+    unauthenticated do
+      root to: redirect('/users/sign_in')
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
